1a8c65661dd0a529fdd61c1be8c7a3575a0b394d
sunghwan <psung9510@gmail.com>
2023-01-14 22:18:52 +0900

    Build Front-end using Flask

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 from extractors.remoteOk import extract_remoteOk_jobs
+from file import save_to_file
 
-keyword = input("what do you want to search for?")
+app = Flask("JobScrapper")
 
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-remote_ok = extract_remoteOk_jobs(keyword)
+database = {}
 
-jobs = indeed+wwr+remote_ok
 
-file = open(f"{keyword}.csv", "w", encoding="utf-8-sig")
-file.write("Position,Company,Location,URL\n")
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-for job in jobs:
-    file.write(
-        f"{job['title']},{job['company']},{job['location'],{job['link']}}\n")
 
-file.close()
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword in database:
+        jobs = database[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        remote_ok = extract_remoteOk_jobs(keyword)
+        jobs = indeed+wwr+remote_ok
+        database[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in database:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, database[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
+
+
+app.run("0.0.0.0")
